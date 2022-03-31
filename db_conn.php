b0a83c6d3383a938cbf1028a9f2c5d702773da77
Johnmel <38127581+Johnmel-Manongdo@users.noreply.github.com>
2022-03-31 23:01:13 +0800

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,10 +1,14 @@
 <?php
 
-$sname = "localhost";
-$uname = "root";
-$password = "";
+// $sname = "localhost";
+// $uname = "root";
+// $password = "";
+// $db_name = "db_ecommerce";
 
-$db_name = "db_ecommerce";
+$sname = "remotemysql.com";
+$uname = "USb3Z7cK0J ";
+$password = "Ry2dwTpFfd";
+$db_name = "USb3Z7cK0J ";
 
 $conn  = mysqli_connect($sname, $uname, $password, $db_name);
 
